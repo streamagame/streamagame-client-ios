1cc2f3ced3937bb1b6c4fa63fd5704f939e9b1a7
Justus Beyer <mail@justus-beyer.de>
2015-09-25 14:01:54 +0200

    Initial Commit

---
+++ Lightdroid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lightdroid
+//
+//  Created by Justus Beyer on 25.09.15.
+//  Copyright © 2015 Justus Beyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lightdroid/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lightdroid
+//
+//  Created by Justus Beyer on 25.09.15.
+//  Copyright © 2015 Justus Beyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
